62f619a47ffd45ead02982d315a772ae02e9f9fa
abcdlzy <abcdlzy@icloud.com>
2017-06-21 14:46:35 +0800

    init

---
+++ windows-commands-helper/windows-commands-helper/wch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace windows_commands_helper
+{
+    public partial class wch : Form
+    {
+        public wch()
+        {
+            InitializeComponent();
+        }
+    }
+}
